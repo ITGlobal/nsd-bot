348b4a8b0c964ec52b48ef255ca5d0239c805458
sarychev <sarychev@itglobal.ru>
2017-04-07 17:08:15 +0300

    IE fixes

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,17 +1,17 @@
-$(() => {
+$(function() {
     var chatIcon = $('.chat-icon');
     var chatWindow = $('.chat-window');
     var chatWindowClose = $('.chat-window-close', chatWindow);
 
     chatIcon.delay(500).fadeIn();
 
-    chatIcon.on('click', () => {
+    chatIcon.on('click', function() {
         chatIcon.hide();
         chatWindow.slideDown();
     });
 
-    chatWindowClose.on('click', () => {
-        chatWindow.slideUp(() => {
+    chatWindowClose.on('click', function() {
+        chatWindow.slideUp(function() {
             chatIcon.fadeIn();
         });
     });
