7aa77f9c1dc9a51717a7bd944bc75c944d8bd63e
sarychev <sarychev@itglobal.ru>
2017-04-12 16:22:38 +0300

    improvements

--- NSD.Bot/Dialogs/RootDialog.cs
+++ NSD.Bot/Dialogs/RootDialog.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using NSD.Bot.StateMachine;
@@ -70,8 +66,26 @@ namespace NSD.Bot.Dialogs
             }
 
             // выполнить полнотекстовый поиск
-            await context.PostAsync(Search(((IMessageActivity)context.Activity).Text));
-            await Done(context);
+            var answers = Search(((IMessageActivity) context.Activity).Text);
+            if (answers[0].Score > 80)
+            {
+                var answer = answers[0].Answer;
+                var score = answers[0].Score;
+                var reply = $"{answer}\n\n---\n\n*Релевантность {score:F0}%*";
+                await context.PostAsync(reply);
+                await Done(context);
+            }
+            else if (answers[0].Questions == null)
+            {
+                await context.PostAsync(embeddedAnswers.notFound);
+                await Done(context);
+            }
+            else
+            {
+                var reply = activity.CreateReply();
+                reply.AddKeyboardCard("Утоните, пожалуйста", answers.Select(x => x.Questions[0]));
+                await context.PostAsync(reply);
+            }
         }
 
         private static async Task Done(IDialogContext context)
@@ -80,20 +94,18 @@ namespace NSD.Bot.Dialogs
             context.Done<object>(null);
         }
 
-        private static string Search(string input)
+        private static IList<QnAMakerAnswer> Search(string input)
         {
             var client = new RestClient("https://westus.api.cognitive.microsoft.com/qnamaker/v2.0");
             var request = new RestRequest("/knowledgebases/8619aa1d-fe00-4a68-9ffc-0a2a8979bc29/generateAnswer", Method.POST);
             request.AddHeader("Ocp-Apim-Subscription-Key", "dc6b8b6a65804c21b30ead4225af6f59");
             request.AddJsonBody(new
             {
-                question = input
+                question = input,
+                top = 3
             });
-            var response = client.Execute<dynamic>(request);
-            dynamic answer = response.Data["answers"][0];
-            var score = answer["score"];
-            var ret = score < 45 ? embeddedAnswers.notFound : answer["answer"];
-            return $"{ret}\n(Инфа {score}%)";
+            var response = client.Execute<QnAMakerResponse>(request);
+            return response.Data.Answers;
         }
 
         private async void ShowPromt(IDialogContext context, Prompt prompt, Activity activity)
@@ -102,5 +114,17 @@ namespace NSD.Bot.Dialogs
             reply.AddKeyboardCard(prompt.Question, prompt.Options, prompt.Options.Select(x => x.ToString()));
             await context.PostAsync(reply);
         }
+    }
+
+    public class QnAMakerResponse
+    {
+        public List<QnAMakerAnswer> Answers { get; set; }
+    }
+
+    public class QnAMakerAnswer
+    {
+        public string Answer { get; set; }
+        public List<string> Questions { get; set; }
+        public double Score { get; set; }
     }
 }
