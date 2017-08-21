55eab093cee156864e48c03d1ca83652388a5473
oceandrama <oceandrama@gmail.com>
2017-08-21 16:19:17 +0300

    Added full functionality for searching:  - Possibility to choose concrete knowledge base  - Answers good/bad relevance

--- NSD.Bot2/Dialogs/RootDialog.cs
+++ NSD.Bot2/Dialogs/RootDialog.cs
@@ -14,6 +14,10 @@ namespace NSD.Bot2.Dialogs
     [Serializable]
     public class RootDialog : IDialog<object>
     {
+        private int _badScore = 25;
+        private int _goodScore = 60;
+        private static List<QnAMakerAnswer> lastResults;
+
         public Task StartAsync(IDialogContext context)
         {
             context.Wait(AfterInput);
@@ -37,51 +41,76 @@ namespace NSD.Bot2.Dialogs
                 return;
             }
 
-            // выполнить полнотекстовый поиск
-            var result = SearchAsync(((IMessageActivity)context.Activity).Text).Result;
-            foreach (QnAMakerAnswer kbres in result)
-            {
-                var reply = $"Ответы из базы знаний - {kbres.KB.KBName}:\n\n---\n\n";
-                foreach (var answer in kbres.KnowledgeBaseResult.Answers)
-                {
-                    var score = answer.Score;
-                    if (score >= 25)
-                        reply += $"{answer.Answer}\n\n---\n\n*Релевантность {score:F0}%*";
-                    else
-                        reply += embeddedAnswers.notFound;
-                }
-                await context.PostAsync(reply);
-            }
-            
-            await Done(context);
-            /*if (answers[0].Score > 80)
-            {
-                var answer = answers[0].Answer;
-                var score = answers[0].Score;
-                var reply = $"{answer}\n\n---\n\n*Релевантность {score:F0}%*";
-                await context.PostAsync(reply);
-                await Done(context);
-            }
-            else if (answers[0].Questions == null)
+            List<QnAMakerAnswer> result = new List<QnAMakerAnswer>();
+            if (lastResults != null)
+                result.Add(lastResults.Find(m => m.KB.KBName.ToLowerInvariant() == input));
+            else
+                result = SearchAsync(((IMessageActivity)context.Activity).Text).Result;
+
+            if (!result.Any(m => m.KnowledgeBaseResult.Answers.Any(n => n.Score > _badScore)))
             {
                 await context.PostAsync(embeddedAnswers.notFound);
                 await Done(context);
+                return;
             }
-            else
+
+            var KBWithBadAnswers = result.FindAll(m => m.KnowledgeBaseResult.Answers.Any(n => n.Score > _badScore && n.Score < _goodScore));
+            var KBWithGoodAnswers = result.FindAll(m => m.KnowledgeBaseResult.Answers.Any(n => n.Score >= _goodScore));
+            if (KBWithGoodAnswers != null && KBWithGoodAnswers.Count != 0)
             {
-                var reply = activity.CreateReply();
-                reply.AddKeyboardCard("Утоните, пожалуйста", answers.Select(x => x.Questions[0]));
-                await context.PostAsync(reply);
-            }*/
+                if (KBWithGoodAnswers.Count() > 1)
+                {
+                    var reply = activity.CreateReply();
+                    reply.AddKeyboardCard("Ответ найден в нескольких категориях. Пожалуйста, уточните область поиска", KBWithGoodAnswers.Select(m => m.KB.KBName));
+                    await context.PostAsync(reply);
+                    lastResults = KBWithGoodAnswers;
+
+                    context.Wait(AfterInput);
+                    return;
+                }
+                else
+                    await context.PostAsync(CreateReply(KBWithGoodAnswers[0], _goodScore));
+            }
+            else if (KBWithBadAnswers != null && KBWithBadAnswers.Count != 0)
+                if (KBWithBadAnswers.Count() > 1)
+                {
+                    var reply = activity.CreateReply();
+                    reply.AddKeyboardCard("Ответ найден в нескольких категориях. Пожалуйста, уточните область поиска", KBWithBadAnswers.Select(m => m.KB.KBName));
+                    await context.PostAsync(reply);
+                    lastResults = KBWithBadAnswers;
+                }
+                else
+                    await context.PostAsync(CreateReply(KBWithBadAnswers[0], _badScore));
+
+            await Done(context);
         }
 
-        private static async Task Done(IDialogContext context)
+        private string CreateReply(QnAMakerAnswer qnaAnswer, int minScore)
         {
+            var reply = $"Ответ найден в категории: {qnaAnswer.KB.KBName}";
+            foreach (var answer in qnaAnswer.KnowledgeBaseResult.Answers)
+            {
+                var score = answer.Score;
+                if (score >= minScore)
+                {
+                    reply += $"\n\n---\n\nВопросы:\n\n";
+                    foreach (string question in answer.Questions)
+                        reply += $"{question}\n\n";
+                    reply += $"Ответ:\n\n{answer.Answer}\n\n*Релевантность {score:F0}%*";
+
+                }
+            }
+            return reply;
+        }
+
+        private async Task Done(IDialogContext context)
+        {
+            lastResults = null;
             await context.PostAsync(embeddedAnswers.thank);
             context.Done<object>(null);
         }
 
-        private static async Task<List<QnAMakerAnswer>> SearchAsync(string input)
+        private async Task<List<QnAMakerAnswer>> SearchAsync(string input)
         {
             List<QnAMakerAnswer> result = new List<QnAMakerAnswer>();
             List<KB> KBs = new List<KB>();
@@ -91,17 +120,18 @@ namespace NSD.Bot2.Dialogs
             }
 
             foreach (var KB in KBs)
-            { 
+            {
                 using (var client = new QnaMakerClient("e6a93449fd254d11a7857b500be15be3"))
                 {
                     var res = await client.GenerateAnswer(KB.KBId, input, 3);
                     result.Add(new QnAMakerAnswer(res, KB));
                 }
             }
-            
+
             return result;
         }
 
+        [Serializable]
         public class QnAMakerAnswer
         {
             public GenerateAnswerResponse KnowledgeBaseResult { get; set; }
